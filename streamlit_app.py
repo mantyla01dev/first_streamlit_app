e9d2c63e2a5d999187a136c6ec9ff6822ee1d1f5
mantyla01dev <107750013+mantyla01dev@users.noreply.github.com>
2022-06-18 12:38:51 +0300

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healty Diner')
 streamlit.title('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
